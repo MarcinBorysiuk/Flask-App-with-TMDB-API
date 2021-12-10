77f1503f3dd7d7f28dcc7e1bfd561cb16813e7fa
Marcin Borysiuk <marcinborysiuk94@gmail.com>
2021-12-10 21:30:24 +0100

    added database and create/delete functionality

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, url_for, request, redirect
 import tmdb_client
 from datetime import date
+from flask_sqlalchemy import SQLAlchemy
 
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-FAVOURITES = set()
 
+class FavouriteMovie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer)
 
 
 
@@ -44,21 +50,30 @@ def search():
 def today():
     movies = tmdb_client.airing_today()
     today = date.today()
+    today = today.strftime("%d/%m/%Y")
     return render_template('today.html', movies=movies, today=today)
 
 
 @app.route("/favourites/add", methods=['POST'])
 def add_to_favorites():
+    all_favourites = FavouriteMovie.query.all()
+    FAVOURITES = [movie.movie_id for movie in all_favourites]
     data = request.form
     movie_id = data.get('movie_id')
-    movie_title = data.get('movie_title')
-    if movie_id and movie_title:
-        FAVOURITES.add(movie_id)
+    if movie_id:
+        if int(movie_id) in FAVOURITES:
+            return redirect(url_for('homepage'))
+        else:
+            movie_to_add = FavouriteMovie(movie_id=movie_id)
+            db.session.add(movie_to_add)
+            db.session.commit()         
     return redirect(url_for('homepage'))
 
 
 @app.route('/favourites')
 def favourite_movies():
+    all_favourites = FavouriteMovie.query.all()
+    FAVOURITES = [movie.movie_id for movie in all_favourites]
     if FAVOURITES:
         movies = []
         for movie_id in FAVOURITES:
@@ -66,9 +81,15 @@ def favourite_movies():
             movies.append(movie_details)
     else:
         movies = []
-    
     return render_template('favourites.html', movies=movies)
     
+
+@app.route('/favourites/delete/<int:id>')
+def delete_movie(id):
+    movie_to_delete = FavouriteMovie.query.filter_by(movie_id=id).first()
+    db.session.delete(movie_to_delete)
+    db.session.commit()
+    return redirect(url_for('favourite_movies'))
 
 
 @app.context_processor
